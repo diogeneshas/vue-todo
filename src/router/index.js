203a3e6090bf8bdf17244fae4d2f6b9bf1663104
Diogenes <diogenes.henrique.br@gmail.com>
2021-08-25 17:58:58 -0300

    Menu de navegação e Vue Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Tarefas from '../views/Tarefas.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Tarefas',
+    component: Tarefas
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/sobre',
+    name: 'Sobre',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
